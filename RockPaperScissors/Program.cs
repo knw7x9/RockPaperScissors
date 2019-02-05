84d26daa26eb84b6a102643a16b1b9a79bb960a5
knw7x9 <katherinewilsdon@gmail.com>
2019-02-04 21:55:50 -0700

    Create a rock, paper scissors game.
    The user picks a one player game, two players game, or an automatic game. For the one player game, the player chooses rock, paper, or scissors and the computer will randomly roll and the program calculates who wins. For the two players game, both players pick rock, paper, or scissors and the program determines who wins. For the computer vs. computer game, the user sets a number of games between 10 and 10,000 on the trackbar and the program randomizes the rolls and calculates to total wins. The output is the total win statistics for player 1, player 2, and the ties.

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -13,7 +13,7 @@ namespace RockPaperScissors {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmRockPaperScissors());
         }
     }
 }
